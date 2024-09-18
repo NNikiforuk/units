dbd552e433a9799b68e24ee262e40a2bdefe8eac
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-18 17:26:33 +0200

    first commit

--- units/ContentView.swift
+++ units/ContentView.swift
@@ -5,17 +5,60 @@
 //  Created by Natalia Nikiforuk on 18/09/2024.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var unitFrom = "m"
+    @State private var unitTo = "m"
+    @State private var amount = 0.0
+    
+    let units = ["m", "km"]
+    
+    var changingUnits: Double {
+        var result: Double = 0.0
+        
+        if (unitFrom == unitTo) {
+            result = amount
         }
-        .padding()
+        
+        if unitFrom == "m" && unitTo == "km" {
+            result = amount / 1000
+        } else if (unitFrom == "km" && unitTo == "m") {
+            result = amount * 1000
+        }
+        
+        return result
+    }
+    
+    var body: some View {
+        Form {
+            Section("Changing from:") {
+                Picker("Changing from:", selection: $unitFrom) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Changing to:") {
+                    Picker("To:", selection: $unitTo) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            
+            Section("Value:") {
+                TextField("Value", value: $amount, format: .number)
+            }
+            
+            Section {
+                Text(changingUnits, format: .number)
+            }
+            
+            }
     }
 }
 
