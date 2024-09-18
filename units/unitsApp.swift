d8d0fed5e0177ee7be7993888c0af4c96cb8ef3c
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-18 15:50:34 +0200

    Initial Commit

---
+++ units/unitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitsApp.swift
+//  units
+//
+//  Created by Natalia Nikiforuk on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct unitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
